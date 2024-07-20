eca166c90a6e40d799cc6be5cb520cf9062011fa
Jerry Wang <x.jerry.wang@gmail.com>
2024-07-20 13:06:19 +0800

    perf: add tooltip for statusbar

--- src/StatusBar.ts
+++ src/StatusBar.ts
@@ -13,12 +13,14 @@ export class StatusBar implements Disposable {
   constructor() {
     this._status = window.createStatusBarItem(StatusBarAlignment.Right, 1000)
     this._status.text = '$(server) VitePress'
+    this._status.tooltip = 'Click to start'
     this._status.show()
     this._status.command = Commands.toggle
   }
 
   started() {
     this._status.text = '$(server) VitePress'
+    this._status.tooltip = 'Click to stop'
     this._status.color = new ThemeColor(
       'terminalCommandDecoration.successBackground'
     )
@@ -26,10 +28,12 @@ export class StatusBar implements Disposable {
 
   spinning() {
     this._status.text = '$(sync~spin) VitePress'
+    this._status.tooltip = 'Starting the VitePress server'
   }
 
   stopped() {
     this._status.text = '$(server) VitePress'
+    this._status.tooltip = 'Click to start'
     this._status.color = undefined
   }
 
